e7b42ff2fe527a0a4d860f5765e8dab2ce605ccf
rgiuliana <riccardo.mescoli@unimore.it>
2026-01-30 19:45:43 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-"""HyperBO.
+"""GPax.
 
 See more details in the
-[`README.md`](https://github.com/google-research/hyperbo).
+[`README.md`](https://github.com/google-research/gpax).
 """
 
 from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='hyperbo',
+    name='gpax',
     version='0.0.1',
-    description='hyperbo',
-    author='HyperBO Team',
+    description='gpax',
+    author='Google Research',
     author_email='wangzi@google.com',
-    url='http://github.com/google-research/hyperbo',
+    url='https://github.com/google-research/gpax',
     license='Apache 2.0',
     packages=find_packages(),
     install_requires=[
